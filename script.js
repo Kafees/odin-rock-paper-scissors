73e60c354efe9a87fa6d79d58ee301dee82f0ed2
filip_kafka <filip.kafka@gmail.com>
2024-12-19 19:32:38 +0100

    add user input with validation

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length);
     return choices[choice];
 }
+
+function getHumanChoice() {
+    let choice = prompt("Do you play rock, paper, or scissors?");
+    if (choices.includes(choice)) {
+        return choice;
+    } else {
+        console.log("Wrong input");
+    }   
+}
