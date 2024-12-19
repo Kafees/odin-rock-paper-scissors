ee19f8e1acafdc174d17f3b3eb4a350cd7d028ad
filip_kafka <filip.kafka@gmail.com>
2024-12-19 19:17:26 +0100

    add computer choice generation

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hi")
+const choices = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * choices.length);
+    return choices[choice];
+}
